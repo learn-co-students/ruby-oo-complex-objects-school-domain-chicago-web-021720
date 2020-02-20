af07489fb537882c5d48fc0dd59860b1a791bf69
owenabbott <ocabbott@gmail.com>
2020-02-20 10:32:10 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+require 'pry'
+class School
+    attr_accessor :roster, :student, :grade, :roster
+
+    def initialize(name)
+        @name=name
+        @roster=Hash.new
+
+    end
+
+    def add_student(student, grade)
+        if @roster[grade]==nil
+            @roster[grade]=[student]
+        else
+            @roster[grade].push student    
+        end        
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        newRoster=Hash.new
+        new_list=@roster.keys.sort
+        for i in new_list
+            roster.each do |key, value|
+                if i==key
+            newRoster[i]=roster[i].sort
+                end
+            end
+        end
+        return newRoster
+    end
+end
