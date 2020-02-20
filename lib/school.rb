2c72beee6588f2c55e94e6d7f744c8744f714210
deber116 <deber116@gmail.com>
2020-02-19 23:43:02 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require 'pry'
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster 
+        @roster
+    end
+
+    def add_student(name, grade)
+        if @roster[grade]
+            @roster[grade] << name
+        else 
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+
+    def grade(key)
+        @roster[key]
+    end
+
+    def sort
+        result = @roster.reduce({}) do |memo, (grade,name_array)|
+            memo[grade] = name_array.sort
+            memo
+        end
+    end
+end
