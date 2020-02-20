91255ba3f93eb99bf592523d0e4f1b9724091f1c
cooljasonmelton <cooljasonmelton>
2020-02-19 18:27:43 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+
+class School
+    attr_reader :roster
+
+    def initialize(name)
+        @roster = {}
+    end 
+    def add_student(name, grade)
+        if @roster[grade]
+            @roster[grade] << name
+        else 
+        @roster[grade] = [name]
+        end 
+    end 
+
+    def grade(number)
+        @roster[number]
+    end 
+
+    def sort
+        @roster.reduce({}) do |memo, (grade, student)|
+            memo[grade] = student.sort
+            memo
+        end 
+    end 
+
+     
+end 
